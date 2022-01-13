ca355e9950b5cc12bd3744ed2ee97ee55211066e
WebStudioFlip <73443296+WebStudioFlip@users.noreply.github.com>
2022-01-13 17:20:54 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryItemsHtml = galleryItems.map(el => `<div class="gallery__item">
+<a class="gallery__link" href=${el.original}>
+  <img
+    class="gallery__image"
+    src=${el.preview}
+    data-source=${el.original}
+    alt=${el.description}
+  />
+</a>
+</div>`).join("")
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemsHtml)
